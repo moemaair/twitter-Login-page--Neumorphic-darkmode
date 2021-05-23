d1b098a6ba342766726d88a8ffd4aab650ce1671
Mohamed Ibrahim <56992198+moemaair@users.noreply.github.com>
2021-05-23 11:47:12 +0300

    Add files via upload

---
+++ twitter-neumophic/app.js
@@ -0,0 +1,22 @@
+let btn=document.querySelector(".btn");
+
+btn.addEventListener("mouseover", ()=>{
+  btn.style.color='white';
+
+btn.style.boxShadow="-4px -4px 20px -9px rgba(255,255,255,80%)";
+
+});
+
+btn.addEventListener("mouseleave", ()=>{
+  btn.style.color='black';
+
+  btn.style.boxShadow="none";
+
+});
+
+            
+ 
+
+
+
+
